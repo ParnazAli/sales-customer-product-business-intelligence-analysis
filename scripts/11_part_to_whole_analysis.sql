5addd1a9371914fe9d5e7bc1da0302be08256b9e
Parnaz Ali <Parnazali1383@gmail.com>
2026-06-07 12:43:41 +0330

    Update 11_part_to_whole_analysis.sql

--- scripts/11_part_to_whole_analysis.sql
+++ scripts/11_part_to_whole_analysis.sql
@@ -1,7 +1,8 @@
 WITH category_sales AS (
     SELECT
         p.category,
-        SUM(f.sales_amount) AS total_sales
+        SUM(f.sales_amount) AS total_sales,
+        COUNT(DISTINCT p.product_key) AS total_products
     FROM gold.fact_sales f
     LEFT JOIN gold.dim_products p
         ON p.product_key = f.product_key
@@ -10,7 +11,14 @@ WITH category_sales AS (
 SELECT
     category,
     total_sales,
+    total_products,
     SUM(total_sales) OVER () AS overall_sales,
-    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
+    SUM(total_products) OVER () AS overall_products,
+    ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS revenue_percentage,
+    ROUND((CAST(total_products AS FLOAT) / SUM(total_products) OVER ()) * 100, 2) AS catalog_percentage,
+    ROUND(
+        (CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) /
+        (CAST(total_products AS FLOAT) / SUM(total_products) OVER ()), 2
+    ) AS revenue_efficiency_ratio
 FROM category_sales
-ORDER BY total_sales DESC;
+ORDER BY revenue_efficiency_ratio DESC;
